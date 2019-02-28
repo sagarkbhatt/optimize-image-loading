5bd7d248ac0c39fa89aff636d1aa7705f11621d6
sagarkbhatt <sagarkbhatt95@gmail.com>
2019-02-28 17:41:15 +0530

    Add intersection observer

--- main.js
+++ main.js
@@ -2,24 +2,52 @@
 
     var lazyLoadBgImg = function() {
 
+        var observer = null;
+        
         var init = function() {
 
             onContentLoad = onContentLoad.bind( this );
+            onIntersect   = onIntersect.bind( this );
+            initIO();
             document.addEventListener( 'DOMContentLoaded', onContentLoad.bind( this ) );
         };
 
         var onContentLoad = function() {
 
-            console.log( ' hi' );
             var selector = '[data-lazy-bg-img]'
             var elms = document.querySelectorAll( selector );
 
             for( var i = 0; i < elms.length; i++ ) {
 
-                var imgSrc = elms[i].dataset.lazyBgImg;
-                preLoadImage( elms[i], imgSrc )
+                observer.observe( elms[i] );
             }
         }
+
+        var initIO = function() {
+
+            var options = {
+                root: null,
+                rootMargin: '150px 0px 0px 0px',
+                threshold: [ 0.2 ]
+            };
+    
+            observer = new IntersectionObserver( onIntersect, options );
+        }
+
+        var onIntersect = function( entries ) {
+
+            for( var j = 0; j < entries.length; j++ ) {
+
+                if ( entries[j].intersectionRatio > 0 ) {
+
+                    // Stop watching and load the image
+                    observer.unobserve(entries[j].target);
+                    var imgSrc = entries[j].target.dataset.lazyBgImg;
+                    preLoadImage(entries[j].target, imgSrc);
+                }
+
+            }          
+         }
 
         var preLoadImage = function( imgElm, imgSrc ) {
 
