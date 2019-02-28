39a5fe9ad3746f961fe61565cc967101a38562d3
sagarkbhatt <sagarkbhatt95@gmail.com>
2019-02-28 16:53:00 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,48 @@
+( function () {
+
+    var lazyLoadBgImg = function() {
+
+        var init = function() {
+
+            onContentLoad = onContentLoad.bind( this );
+            
+            if ( 'loading' !== document.readyState ) {
+                onContentLoad();
+            } else {
+                document.addEventListener( 'DOMContentLoaded', onContentLoad.bind( this ) );
+            }
+        };
+
+        var onContentLoad = function() {
+
+            console.log( ' hi' );
+            var selector = '[data-lazy-bg-img]'
+            var elms = document.querySelectorAll( selector );
+
+            for( var i = 0; i < elms.length; i++ ) {
+
+                var imgSrc = elms[i].dataset.lazyBgImg;
+                preLoadImage( elms[i], imgSrc )
+            }
+        }
+
+        var preLoadImage = function( imgElm, imgSrc ) {
+
+            var img = new Image();
+
+            img.onload = function() {
+    
+                imgElm.style.backgroundImage = 'url(' + img.src + ')';
+            }
+
+            img.src = imgSrc;
+        }
+
+        return {
+            init: init
+        };
+    }
+
+    var instance = new lazyLoadBgImg();
+    instance.init();
+} () )
